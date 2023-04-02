a2432403e08af5e2fe38f8d097aa0442b037c988
Nikko2450 <Nikko175012@gmail.com>
2023-04-02 22:55:02 +0300

    video

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,15 @@
-import sayHello from './lib/sayHello';
+document.addEventListener('DOMContentLoaded', () => {
+  const videoWrapper = document.querySelector('.video__wrapper');
+  const playButton = videoWrapper.querySelector('.video__play');
+  const video = videoWrapper.querySelector('.video__source');
 
-sayHello();
+  playButton.addEventListener('click', () => {
+    if (video.paused) {
+      video.play();
+      videoWrapper.classList.add('is-playing');
+    } else {
+      video.pause();
+      videoWrapper.classList.remove('is-playing');
+    }
+  })
+});
