28a749b22956fe74916ab3b02da0e104707f7db7
Nikko2450 <Nikko175012@gmail.com>
2023-04-05 22:50:11 +0300

    professional-team

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,21 @@ import Filterizr from 'filterizr'
 const options = {layout: 'sameWidth'};
 const filterizr = new Filterizr('.gallery', options);
 
+const toggleActiveItem = () => {
+  const controls = document.querySelector('.filtr-controls');
+  const controlItem = controls.querySelectorAll('li.filtr-control');
+
+  [...controlItem].forEach(item => {
+    item.addEventListener('click', () => {
+      const current = controls.querySelector('li.active');
+      if (current) current.classList.remove('active');
+      item.classList.add('active');
+    });
+  });
+}
+
+toggleActiveItem();
+
 document.addEventListener('DOMContentLoaded', () => {
   const videoWrapper = document.querySelector('.video__wrapper');
   const playButton = videoWrapper.querySelector('.video__play');
